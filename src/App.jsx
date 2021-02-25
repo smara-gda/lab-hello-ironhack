6860e87d72feb62364cd89800b3b72f977f348ea
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2021-02-25 19:57:45 +0100

    adding iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-
+// import logo from './logo.svg';
+import './index.css';
 import './App.css';
 
 class App extends Component {
@@ -8,19 +8,45 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="App-menu"> 
+            <img src='./images/menu-top.svg' alt="menu-icon" />
+          </div>
+          <div className="App-hack-logo">
+            <img src='./images/ironhack-logo.svg' alt="ironhack-logo" />
+          </div>
+          
+          <div  className="App-heading"> 
+            <h1>
+            Say hello to ReactJS.
+            </h1>
+            <p className="App-dscrp">You will learn a Frontend framework from scratch, to become a Ninja Developer</p>
+            <button> Awesome! </button>
+          </div>
         </header>
+
+        <div className="App-section-wrapper">
+          <div className="App-blocks">
+            <img src='./images/icon1.png' alt="description "/>
+            <h3>Declarative </h3>
+            <p> React makes it painles to create interactive UIs. </p>
+          </div>
+          <div  className="App-blocks">
+            <img src='./images/icon2.png' alt="description "/>
+            <h3>Components </h3>
+            <p> Build encapsulated components that manage their sate.</p>
+          </div>
+          <div  className="App-blocks">
+            <img src='./images/icon3.png' alt="description "/>
+            <h3> Single-Way</h3>
+            <p> A set of immutable values are passed to the components</p>
+          </div>
+          <div  className="App-blocks">
+            <img src='./images/icon4.png' alt="description "/>
+            <h3> JSX</h3>
+            <p> Statically-typed, design to run on modern browsers.</p>
+          </div>
+
+        </div>
       </div>
     );
   }
